352dd8998d4d4048b0ddbad1f204a8be9c691695
zypex14 <zypex14@gmail.com>
2021-09-05 13:07:41 -0500

    Added default methods to get the outputs of each of the endpoints of a function's domain

--- src/utils/math/BoundedFunction.java
+++ src/utils/math/BoundedFunction.java
@@ -3,5 +3,13 @@ package utils.math;
 public interface BoundedFunction<T> extends Function<T> {
     double upperBound();
     double lowerBound();
+
+    default T getUpper(){
+        return get(upperBound());
+    }
+    default T getLower(){
+        return get(lowerBound());
+    }
+
     BoundedFunction<T> offset(double offset);
 }
