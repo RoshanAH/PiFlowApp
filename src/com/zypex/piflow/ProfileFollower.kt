f658a0ff24d8f76f85c79a6dbbabb5886597f8fa
Roshan <roshanahegde@gmail.com>
2022-02-14 13:29:41 -0600

    Overhauled class to follow a time based pace rather than reactively

--- src/com/zypex/piflow/ProfileFollower.kt
+++ src/com/zypex/piflow/ProfileFollower.kt
@@ -1,89 +1,96 @@
 package com.zypex.piflow
 
-import com.zypex.piflow.profile.*
-import utils.math.SingleBoundedFunction
-import kotlin.math.pow
+import com.zypex.piflow.profile.Derivatives
+import com.zypex.piflow.profile.DoubleProfile
 
-class ProfileFollower(var veloPIDF: PIDFConstants, var getPosition: () -> Double, var follow: (Double) -> Unit) {
+class ProfileFollower(var pidf: PIDFConstants, var getPosition: () -> Double, var follow: (Double) -> Unit) {
 
     private var lastError = 0.0
     private var lastTime = System.nanoTime() * 1e-9
+    private var startTime = lastTime
+
     private var deltaError = 0.0
     private var lastPos = 0.0
     private var pos = 0.0
     private var deltaTime = 0.0
 
+    var targetPosition: Derivatives<Double> = Derivatives(0.0, 0.0, 0.0, 0.0)
+        private set
     var t = 0.0
         private set
 
     var profile: DoubleProfile = DoubleProfile()
         set(value) {
-            segNum = 0
+            startTime = System.nanoTime() * 1e-9
+//            segNum = 0
             error = 0.0
             totalError = 0.0
+            deltaError = 0.0
             field = value
         }
-
-    var getVelocity: () -> Double = { (pos - lastPos) / deltaTime }
-
-    var segNum = 0
-        private set
 
     private var error = 0.0
     private var totalError = 0.0
 
     fun update() {
         val time = System.nanoTime() * 1e-9
+        t = time - startTime
         deltaTime = time - lastTime
 
-
         pos = getPosition()
-        val vel = getVelocity()
 
-        t = getT(pos)
+        targetPosition = profile.bounded(t)
 
-        val derivatives = profile(t)
-
-        error = derivatives.velocity - vel
+        error = targetPosition.position - pos
         deltaError = (error - lastError) / deltaTime
         totalError += error
 
-        follow(veloPIDF.p * error + veloPIDF.i * totalError - veloPIDF.d * deltaError + veloPIDF.f * derivatives.acceleration)
+        val pidPower = pidf.p * error + pidf.i * totalError + pidf.d * deltaError
+        val ffPower = pidf.fV * targetPosition.velocity + pidf.fA * targetPosition.acceleration + pidf.fJ * targetPosition.jerk
+
+        follow(pidPower + ffPower)
 
         lastTime = time
         lastPos = pos
     }
 
-    private fun getT(pos: Double): Double {
-        if (profile.functions.isEmpty()) return 0.0
-        if (pos < profile.functions[segNum].lower.position) return profile.functions[segNum].lowerBound()
+//    fun getT(pos: Double): Double {
+//        if (profile.functions.isEmpty()) return 0.0
+//        if (pos < profile.lower.position) return profile.lowerBound()
+//
+//        var lowest = profile.lowerBound()
+//
+//        for (i in 0 until profile.functions.size) {
+//            val f = profile.functions[i]
+//            if (pos <= f.upper.position) {
+//                val lower = f.lower
+//                val lBound = f.lowerBound()
+//                val j = lower.jerk
+//                val a = lower.acceleration
+//                val v = lower.velocity
+//                val p = lower.position
+//
+//                val function: (Double) -> Double = { j / 6.0 * (it - lBound).pow(3.0) + a / 2.0 * (it - lBound).pow(2.0) + v * (it - lBound) + p }
+//                val derivative: (Double) -> Double = { j / 2.0 * (it - lBound).pow(2.0) + a * (it - lBound) + v }
+//
+//                val t = newtonMethodSolve(
+//                    pos,
+//                    function,
+//                    derivative,
+//                    f.lowerBound(),
+//                    f.upperBound(),
+//                    initialGuess = t.coerceAtLeast(1e-5)
+//                )
+//
+//                if(t)
+//            }
+//
+//            if (segNum < profile.functions.size - 1) segNum++
+//        }
+//        return profile.upperBound()
+//    }
 
-        for (i in segNum until profile.functions.size) {
-            val f = profile.functions[i]
-            if (pos <= f.upper.position) {
-                val lower = f.lower
-                val lBound = f.lowerBound()
-                val j = lower.jerk
-                val a = lower.acceleration
-                val v = lower.velocity
-                val p = lower.position
-
-                val function: (Double) -> Double = { j / 6.0 * (it - lBound).pow(3.0) + a / 2.0 * (it - lBound).pow(2.0) + v * (it - lBound) + p }
-                val derivative: (Double) -> Double = { j / 2.0 * (it - lBound).pow(2.0) + a * (it - lBound) + v }
-
-                return newtonMethodSolve(
-                    pos,
-                    function,
-                    derivative,
-                    f.lowerBound(),
-                    f.upperBound(),
-                    initialGuess = t.coerceAtLeast(1e-5)
-                )
-            }
-
-            if (segNum < profile.functions.size - 1) segNum++
-        }
-
-        return profile.upperBound()
+    fun reset(){
+        profile = profile
     }
 }
