dea86a538190de47614bc4bf58624236d3503589
Roshan <roshanahegde@gmail.com>
2022-02-14 13:29:10 -0600

    Values are accessible now

--- src/com/zypex/piflow/profile/ProfileOverconstrainedException.kt
+++ src/com/zypex/piflow/profile/ProfileOverconstrainedException.kt
@@ -1,3 +1,9 @@
 package com.zypex.piflow.profile
 
-class ProfileOverconstrainedException(message: String) : IllegalArgumentException(message)
+class ProfileOverconstrainedException(
+    val dist: Double,
+    val deltaVel: Double,
+    val minDisplacement: Double,
+    val minVeloChange: Double,
+) : IllegalArgumentException("Distance $dist is too small to change $deltaVel in velocity. " +
+"Distance must either be $minDisplacement or the change in velocity must be $minVeloChange")
